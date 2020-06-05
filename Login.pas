843aa270f0fe539a17fce99dce2862eab880e297
FFoell <florian_foell@yahoo.de>
2020-06-05 20:47:08 +0200

    Login(FMX)

---
+++ Login.pas
@@ -0,0 +1,26 @@
+unit Login;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, LoginFrame;
+
+type
+  TForm1Login = class(TForm)
+    StyleBook1: TStyleBook;
+    Frame11: TFrame1;
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1Login: TForm1Login;
+
+implementation
+
+{$R *.fmx}
+
+end.
