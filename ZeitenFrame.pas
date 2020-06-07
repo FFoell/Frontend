a4591c7521eec806d8554d048889f8b2384b02b6
MaxSeifarth <38668565+Scoarden@users.noreply.github.com>
2020-06-07 18:37:12 +0200

    Bearbeitung "Zeiten einsehen"

--- ZeitenFrame.pas
+++ ZeitenFrame.pas
@@ -4,10 +4,25 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
-  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls;
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, FMX.Layouts, FMX.MultiView, FMX.Controls.Presentation;
 
 type
   TZeitenFrame1 = class(TFrame)
+    Hintergrund: TImage;
+    ToolBar1: TToolBar;
+    Label2: TLabel;
+    Button2: TButton;
+    ZurückButton: TButton;
+    DrawerMultiView2: TMultiView;
+    VertScrollBox1: TVertScrollBox;
+    GridPanelLayout1: TGridPanelLayout;
+    RectUnbestätigt: TRectangle;
+    ImageUnbestätigt: TImage;
+    RectBestätigt: TRectangle;
+    ImageBestätigt: TImage;
+    RectAbgelehnt: TRectangle;
+    ImageAbgelehnt: TImage;
   private
     { Private-Deklarationen }
   public
